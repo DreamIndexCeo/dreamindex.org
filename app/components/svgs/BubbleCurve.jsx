c4d1e1ddea2776dd718610efd2c94e0fcd72b64d
jackflap <auelias05@gmail.com>
2024-01-08 19:26:52 -0500

    added curves to website

---
+++ app/components/svgs/BubbleCurve.jsx
@@ -0,0 +1,16 @@
+import styles from './bubbleCurve.css'
+const BubbleCurve = ({color}) => {
+    return (
+        <div class="bubbleCurve">
+    <svg fill={color} data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z"></path>
+    </svg>
+</div>
+    );
+};
+
+export function links(){
+    return [{rel: 'stylesheet', href: styles }];
+}
+
+export default BubbleCurve;
--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,4 +1,9 @@
 import NavBar, { links as navBarLinks } from "../components/NavBar";
+import BubbleCurve, {links as bubbleCurveLinks} from "../components/svgs/BubbleCurve";
+import TopBookCurve, {links as topBookCurveLinks} from "../components/svgs/TopBookCurve";
+import BottomBookCurve, {links as bottomOfBookCurveLinks} from "../components/svgs/BottomOfBookCurve";
+
+
 
 export const meta = () => {
   return [{ title: "DreamIndex" }];
@@ -12,8 +17,10 @@ export default function Index() {
         <section>
           <h1>section 1</h1>
         </section>
+         <BubbleCurve color='#111111' />
       </div>
-      
+     
+     <div>
       <section className="services">
         <div>
           <h1>Row</h1>
@@ -22,27 +29,31 @@ export default function Index() {
           <h1>Services</h1>
         </div>
         <div>
-          <h1>Astronaut</h1>
+          <h1>Astronaut</h1>     
         </div>
+        <BubbleCurve color='#D9D9D9' />
       </section>
+     
+    </div>
 
-       <section className="websiteProcess">
+    <section className="websiteProcess">
         <div>
           <h1>section 3</h1>
         </div>
         <div>
           <h1>section 3</h1>
         </div>      
-      </section>
+    </section>
 
-       <section className="dreamsite">
-        <div>
-          <h1>section 4</h1>
-        </div>
-        <div>
-          <h1>section 4</h1>
-        </div>      
-      </section>
+    <section className="dreamsite">
+      <div>
+        <h1>section 4</h1>
+      </div>
+      <div>
+        <h1>section 4</h1>
+      </div>  
+      <BottomBookCurve />
+    </section>
 
        <section className="dica">
         <div>
@@ -53,7 +64,8 @@ export default function Index() {
         </div> 
         <div>
           <h1>section 5</h1>
-        </div>      
+        </div>    
+        <TopBookCurve />  
       </section>
       
     </div>
@@ -65,5 +77,5 @@ export default function Index() {
 
 // maps out all the links the components contains
 export function links() {
-  return [...navBarLinks()];
+  return [...navBarLinks(), ...bubbleCurveLinks(), ...topBookCurveLinks(), ...bottomOfBookCurveLinks()];
 }
