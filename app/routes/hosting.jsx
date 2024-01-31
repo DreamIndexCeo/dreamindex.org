a831ebfb40fd67173008e175bebd5efecc193868
index <kavinlajara@gmail.com>
2024-01-31 17:30:50 -0500

    Day 2

--- app/routes/hosting.jsx
+++ app/routes/hosting.jsx
@@ -15,85 +15,100 @@ export default function Services() {
           Now offering the first 30 days free for your first Company made
           website
         </p>
+      </section>
+      
+      <section className="switchContainer">
+        <p className="subTitle">
+          Monthly / Yearly
+        </p>
         <label class="switch">
-          <input class="toggle" type="checkbox">
-          <span class="slider"></span>
-          <span class="card-side"></span>
+            <input class="toggle" type="checkbox"/>
+            <span class="slider"></span>
+            <span class="card-side"></span>
         </label>
       </section>
 
-      <section className="hostingOptions">
-        <div className="basic priceContainer">
-          <h3>BASIC</h3>
-          <div className="priceRow">
-            <h3 className="price">$15</h3>
-            <h3 className="price2">/m</h3>
+      <section className="hostingContainer">
+        <section className="hostingOptions">
+          <div className="basic priceContainer">
+            <h3>BASIC</h3>
+            <div className="priceRow">
+              <h3 className="price">$15</h3>
+              <h3 className="price2">/m</h3>
+            </div>
+            <p className="planList">
+              <ul>
+                <li>Standard Performance</li>
+                <li>24/7 Website Hosting</li>
+                <li>Domain Name</li>
+                <li>Wesbite Security</li>
+                <li>9/5 Live Support</li>
+              </ul>
+            </p>
+            <p>*Fee $50 for wesbite changes</p>
           </div>
-          <p className="planList">
-            <ul>
-              <li>Standard Performance</li>
-              <li>24/7 Website Hosting</li>
-              <li>Domain Name</li>
-              <li>Wesbite Security</li>
-              <li>9/5 Live Support</li>
-            </ul>
-          </p>
-          <p>*Fee $50 for wesbite changes</p>
-        </div>
-        <div className="advance priceContainer">
-          <h3>ADVANCED</h3>
-          <div className="priceRow">
-            <h3 className="price">$35</h3>
-            <h3 className="price2">/m</h3>
+          <div className="advance priceContainer">
+            <h3>ADVANCED</h3>
+            <div className="priceRow">
+              <h3 className="price">$35</h3>
+              <h3 className="price2">/m</h3>
+            </div>
+            <p className="planList">
+              <ul>
+                <li>Standard Performance</li>
+                <li>24/7 Website Hosting</li>
+                <li>Domain Name</li>
+                <li>Wesbite Security</li>
+                <li>9/5 Live Support</li>
+                <li> + Web Editior (Desktop version)</li>
+                <li> + Database Hosting</li>
+                <li> + Appointment Manager</li>
+              </ul>
+            </p>
           </div>
-          <p className="planList">
-            <ul>
-              <li>Standard Performance</li>
-              <li>24/7 Website Hosting</li>
-              <li>Domain Name</li>
-              <li>Wesbite Security</li>
-              <li>9/5 Live Support</li>
-              <li> + Web Editior (Desktop version)</li>
-              <li> + Database Hosting</li>
-              <li> + Appointment Manager</li>
-            </ul>
-          </p>
-        </div>
-        <div className="master priceContainer">
-          <h3>MASTER</h3>
-          <div className="priceRow">
-            <h3 className="price">$60</h3>
-            <h3 className="price2">/m</h3>
+          <div className="master priceContainer">
+            <h3>MASTER</h3>
+            <div className="priceRow">
+              <h3 className="price">$60</h3>
+              <h3 className="price2">/m</h3>
+            </div>
+            <p className="planList">
+              <ul>
+                <li>+ Increased Performance</li>
+                <li>24/7 Website Hosting</li>
+                <li>Domain Name</li>
+                <li>Wesbite Security</li>
+                <li>+ 24/7 Live Support</li>
+                <li>+ Web Editior (Desktop version & App Version)</li>
+                <li>Database Hosting</li>
+                <li>Appointment Manager</li>
+                <li>+ 1 Extra Website and Database hosting</li>
+              </ul>
+            </p>
           </div>
-          <p className="planList">
-            <ul>
-              <li>+ Increased Performance</li>
-              <li>24/7 Website Hosting</li>
-              <li>Domain Name</li>
-              <li>Wesbite Security</li>
-              <li>+ 24/7 Live Support</li>
-              <li>+ Web Editior (Desktop version & App Version)</li>
-              <li>Database Hosting</li>
-              <li>Appointment Manager</li>
-              <li>+ 1 Extra Website and Database hosting</li>
-            </ul>
-          </p>
-        </div>
-        <div className="corporation priceContainer">
-          <h3>Corporation</h3>
-          <div className="priceRow">
-            <h3 className="price">$???</h3>
-            <h3 className="price2">/m</h3>
+          <div className="corporation priceContainer">
+            <h3>Corporation</h3>
+            <div className="priceRow">
+              <h3 className="price">$???</h3>
+              <h3 className="price2">/m</h3>
+            </div>
           </div>
-        </div>
+
+          
+        </section>
+
+        <a class="fancy" href="#">
+          <span class="top-key"></span>
+          <span class="text">Get A Quote</span>
+          <span class="bottom-key-1"></span>
+          <span class="bottom-key-2"></span>
+        </a>
       </section>
 
       <section>
         <div className="blueDiv">
-          <div>
-            <img src="./imgs/software_mockup.png" />
-          </div>
-          <div>
+          <div class="img"></div>
+          <div class="text">
             <h1>Make your Changes with DreamSite</h1>
             <p>
               No need to contact any Developers or Designers to make any simple
